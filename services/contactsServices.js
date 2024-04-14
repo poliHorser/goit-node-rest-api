5f231ffb315588e3671dc5d13864bd1cea87413f
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-04-14 22:01:44 +0300

    f

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -4,7 +4,7 @@ export const listContacts = (filter ={}, setting = {}) => Contact.find(filter, "
 export const countContacts = filter => Contact.countDocuments(filter);
 export const addContact = (data) => Contact.create(data)
 
-export const updateStatusContact = (id, data) => Contact.findByIdAndUpdate(id, data);
+export const updateStatusContact = (id, data) => Contact.findOneAndUpdate(id, data);
 
 
 export const getContactByFilter = filter =>  Contact.findOne(filter)
